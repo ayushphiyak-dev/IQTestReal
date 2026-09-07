644587dfbee92df406a2eb32a3cb469ca3aa0b1c
Ayush phiyak <ayushphiyak.cse2025@nsec.ac.in>
2026-09-08 00:05:13 +0530

    Add consent-gated GA4 analytics support

---
+++ components/analytics/AnalyticsScript.tsx
@@ -0,0 +1,12 @@
+'use client';
+
+import Script from 'next/script';
+import { useConsent } from '@/components/consent/ConsentProvider';
+
+export function AnalyticsScript() {
+  const { consent } = useConsent();
+  const measurementId = process.env.NEXT_PUBLIC_GA_ID;
+  if (!measurementId || consent !== 'granted') return null;
+  return <><Script src={`https://www.googletagmanager.com/gtag/js?id=${measurementId}`} strategy="afterInteractive" /><Script id="iqtestreal-ga4" strategy="afterInteractive">{`window.dataLayer=window.dataLayer||[];function gtag(){dataLayer.push(arguments);}gtag('js',new Date());gtag('config','${measurementId}',{anonymize_ip:true});`}</Script></>;
+}
+
