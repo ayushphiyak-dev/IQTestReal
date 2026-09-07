b81f8373a5d6bc199bf1822f91568144fd8870b0
Ayush phiyak <ayushphiyak.cse2025@nsec.ac.in>
2026-09-07 21:27:08 +0530

    Use supplied brain illustration for site branding

--- components/BrandMark.tsx
+++ components/BrandMark.tsx
@@ -1,6 +1,5 @@
 import Image from 'next/image';
 
 export function BrandMark() {
-  return <Image className="brand-mark" src="/iqtestreal-brain.svg" width={36} height={36} alt="IQTestReal brain logo" priority unoptimized />;
+  return <Image className="brand-mark" src="/iqtestreal-brain.png" width={36} height={36} alt="IQTestReal brain logo" priority unoptimized />;
 }
-
