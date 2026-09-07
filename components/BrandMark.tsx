aa4adf721a9f2db41786f733f7139f3233b4904d
Ayush phiyak <ayushphiyak.cse2025@nsec.ac.in>
2026-09-07 23:31:36 +0530

    Remove existing source before restoring Sites build (components/BrandMark.tsx)

--- components/BrandMark.tsx
+++
@@ -1,5 +0,0 @@
-import Image from 'next/image';
-
-export function BrandMark() {
-  return <Image className="brand-mark" src="/iqtestreal-brain.png" width={36} height={36} alt="IQTestReal brain logo" priority unoptimized />;
-}
