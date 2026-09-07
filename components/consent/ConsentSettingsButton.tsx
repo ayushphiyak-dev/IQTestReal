451cdcf1978362ab72bead195282bfc98b8b7671
Ayush phiyak <ayushphiyak.cse2025@nsec.ac.in>
2026-09-07 23:31:58 +0530

    Remove existing source before restoring Sites build (components/consent/ConsentSettingsButton.tsx)

--- components/consent/ConsentSettingsButton.tsx
+++
@@ -1,9 +0,0 @@
-'use client';
-
-export function ConsentSettingsButton() {
-  function openSettings() {
-    window.dispatchEvent(new CustomEvent('iqtestreal:open-consent-settings'));
-    if (typeof (window as Window & { __tcfapi?: unknown }).__tcfapi === 'undefined') alert('Consent settings will become available after the site owner connects a certified consent platform.');
-  }
-  return <button className="text-button" type="button" onClick={openSettings}>Privacy settings</button>;
-}
