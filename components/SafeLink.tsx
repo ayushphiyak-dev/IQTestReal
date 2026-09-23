44e0f3a774e418b531a240e39036b91d76783ce3
Ayush phiyak <ayushphiyak.cse2025@nsec.ac.in>
2026-09-24 00:06:41 +0530

    Scope PR to SEO and content changes

--- components/SafeLink.tsx
+++ components/SafeLink.tsx
@@ -4,9 +4,5 @@ type SafeLinkProps = AnchorHTMLAttributes<HTMLAnchorElement> & { href: string };
 
 /** Native anchors keep internal navigation reliable across every deployment target. */
 export function SafeLink({ href, children, ...props }: SafeLinkProps) {
-  return (
-    <a href={href} {...props}>
-      {children}
-    </a>
-  );
+  return <a href={href} {...props}>{children}</a>;
 }
